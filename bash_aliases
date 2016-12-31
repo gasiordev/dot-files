a8a03d64b91d5c0daa0f44d6c67347aa5b022788
Nicholas Gasior <nicholas@laatu.uk>
2016-12-31 16:39:55 +0000

    Change aliases to not use 'x'.

--- bash_aliases
+++ bash_aliases
@@ -1,12 +1,14 @@
 alias e='vim -u ~/.vimrc'
 
-alias xa='git add'
-alias xc='git commit'
-alias xp='git pull'
-alias xx='git push'
+alias aa='git add'
+alias cc='git commit'
+alias pp='git pull'
+alias oo='git push'
 alias gg='git clone'
-alias xs='git status'
-alias xd='git diff'
+alias ss='git status'
+alias dd='git diff'
+
+alias wc='~/.bash_scripts/wc.sh'
 
 alias c='cat'
 
--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ pwd=$(pwd)
 test -f ~/.bash_aliases && mv ~/.bash_aliases ~/.bash_aliases.bak
 test -f ~/.tmux.conf && mv ~/.tmux.conf ~/.tmux.conf.bak
 test -f ~/.vimrc && mv ~/.vimrc ~/.vimrc.bak
+test -d ~/.bash_scripts && mv ~/.bash_scripts ~/.bash_scripts.bak
 
 cp -rf $pwd/bash_aliases ~/.bash_aliases
 cp -rf $pwd/tmux.conf ~/.tmux.conf
 cp -rf $pwd/vimrc ~/.vimrc
+cp -rf $pwd/bash_scripts ~/.bash_scripts
 
 grep -q 'source ~/.bash_aliases' ~/.profile || echo 'source ~/.bash_aliases' >> ~/.profile
 
