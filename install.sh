43530aecdf512c533525d7974dd69afe65877365
Nicholas Gasior <nicholas@laatu.uk>
2016-12-31 16:23:14 +0000

    Add checking if .bash_aliases isn't already included.

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ test -f ~/.bash_aliases && mv ~/.bash_aliases ~/.bash_aliases.bak
 test -f ~/.tmux.conf && mv ~/.tmux.conf ~/.tmux.conf.bak
 test -f ~/.vimrc && mv ~/.vimrc ~/.vimrc.bak
 
-ln -s $pwd/bash_aliases ~/.bash_aliases
-ln -s $pwd/tmux.conf ~/.tmux.conf
-ln -s $pwd/.vimrc ~/.vimrc
+cp -rf $pwd/bash_aliases ~/.bash_aliases
+cp -rf $pwd/tmux.conf ~/.tmux.conf
+cp -rf $pwd/vimrc ~/.vimrc
+
+grep -q 'source ~/.bash_aliases' ~/.profile || echo 'source ~/.bash_aliases' >> ~/.profile
 
