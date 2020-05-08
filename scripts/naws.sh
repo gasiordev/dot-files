23ddbf6b0a4e62b7495fb11fa6836ffeb836c895
Mikolaj Gasior <mg@f0x.pl>
2020-05-08 15:41:25 +0200

    Add AWS profile to naws script

--- scripts/naws.sh
+++ scripts/naws.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-CACHE_FILE=~/.secretcache/.naws.sh.cache
+CACHE_FILE=~/.secretcache/.naws.sh
 
 do_refresh=no;
+profile="default";
 
 for var in "$@"
 do
   if [[ "x$var" == "x--refresh" ]]; then
     do_refresh=yes
   fi
+  if [[ "x$var" =~ ^x--profile=[a-z0-9]+$ ]]; then
+    profile=${var/--profile=/}
+  fi
 done
 
-if [[ ! -f "$CACHE_FILE" || "$do_refresh" == "yes" ]]; then
-  #aws ec2 describe-instances --max-items=150 --query 'Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key==`Name`]|[0].Value,PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Subnet:SubnetId}' --output=table > "$CACHE_FILE";
-  aws ec2 describe-instances --max-items=150 --output=json > "$CACHE_FILE";
+profile_cache_file="$CACHE_FILE"."$profile"
+
+if [[ ! -f "$profile_cache_file" || "$do_refresh" == "yes" ]]; then
+  aws --profile=$profile ec2 describe-instances --max-items=150 --output=json > "$profile_cache_file";
 fi
 
-if [[ -f "$CACHE_FILE" ]]; then
-  for i in `cat "$CACHE_FILE" | jq -c '.Reservations[].Instances[] | select(.State.Name=="running")' | base64`; do
+if [[ -f "$profile_cache_file" ]]; then
+  for i in `cat "$profile_cache_file" | jq -c '.Reservations[].Instances[] | select(.State.Name=="running")' | base64`; do
     echo ${i} | base64 --decode | jq -r '[.PrivateIpAddress, if .PublicIpAddress == null then "-" else .PublicIpAddress end, (.Tags[] | select(.Key=="Name") | .Value)]' | jq -r 'join(" ")' | awk '{ printf "%-16s %-16s %-50s\n", $1, $2, $3}'
   done
 else
